0b58f31f3d9c50c791ac50554f366aeecc74c3b4
Filype.Ferreira <filype.ferreira@empiricus.com.br>
2024-08-21 20:26:40 -0300

    inicio do projeto

---
+++ js/app.js
@@ -0,0 +1,42 @@
+let totalNoCarrinho = [];
+let carrinhoTotal = document.getElementById('valor-total');
+totalNoCarrinho.push(parseInt(carrinhoTotal.textContent.replace('R$','')));
+
+function adicionar(){
+    let produto = document.getElementById('produto').value;
+    let produtoSemEspaco = produto.split(" - ");
+
+    let descricaoProduto = document.getElementById('lista-produtos');
+    let quantidade = document.getElementById('quantidade').value;
+
+    if(quantidade == ""){
+        alert("Por favor informe um valor!")
+        return false;
+    }
+
+    let valorDoProduto = produtoSemEspaco[1].replace('R$','') * quantidade;
+
+    console.log(parseInt(carrinhoTotal.textContent.replace('R$','')));
+
+    totalNoCarrinho.push(valorDoProduto);
+
+    descricaoProduto.innerHTML = descricaoProduto.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+    <section class="carrinho__produtos__produto">
+      <span class="texto-azul">${quantidade} x</span> ${produtoSemEspaco[0]} <span class="texto-azul">${produtoSemEspaco[1]}</span>
+    </section>
+  </section>`;
+
+    carrinhoTotal.innerHTML = totalNoCarrinho.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+function limpar(){
+    totalNoCarrinho = [];
+
+    let quantidade = document.getElementById('quantidade');
+    quantidade.value = "";
+    let descricaoProduto = document.getElementById('lista-produtos');
+    descricaoProduto.innerHTML = "";
+    let carrinhoTotal = document.getElementById('valor-total');
+    carrinhoTotal.innerHTML =  `0`;
+
+}
